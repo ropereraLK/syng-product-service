7b4c46916e43eee6b2ca161f562248df6e70e859
Rohan Perera <ropereralk@gmail.com>
2023-04-23 11:47:18 +0530

    Phase 01 Implementation

---
+++ src/main/java/io/github/ropereralk/syng/productservice/repository/DatabaseCollection.java
@@ -0,0 +1,59 @@
+package io.github.ropereralk.syng.productservice.repository;
+
+import io.github.ropereralk.syng.productservice.configurations.MongoConfiguration;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
+import io.github.ropereralk.syng.productservice.exception.DocumentNotFoundException;
+
+public class DatabaseCollection {
+
+    @Autowired
+    MongoConfiguration mongoConfiguration;
+
+    public JSONObject find(String key, String value , String collection) throws DocumentNotFoundException {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where(key).is(value));
+
+        try {
+            JSONObject document = mongoConfiguration.mongoTemplate().findOne(query, JSONObject.class, collection);
+
+            if (document == null) {
+                throw new DocumentNotFoundException(HttpStatus.NOT_FOUND, "DB-FIND_ONE" + " : Document Not Found");
+            } else {
+                return document;
+            }
+        }catch (DocumentNotFoundException dne)   {
+            throw dne;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+    public JSONObject findAll(String collection) throws DocumentNotFoundException {
+
+        Query query = new Query();
+
+
+        try {
+            JSONObject document = (JSONObject) mongoConfiguration.mongoTemplate().findAll(JSONObject.class,collection);
+
+            if (document == null) {
+                throw new DocumentNotFoundException(HttpStatus.NOT_FOUND, "DB-FIND_ONE" + " : Document Not Found");
+            } else {
+                return document;
+            }
+        }catch (DocumentNotFoundException dne)   {
+            throw dne;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+
+}
---
+++ src/main/java/io/github/ropereralk/syng/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package io.github.ropereralk.syng.productservice.service;
+
+import io.github.ropereralk.syng.productservice.dto.ProductCategories;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements  ProductService{
+    @Override
+    public List<ProductCategories> getProducCategories() {
+        return null;
+    }
+}
